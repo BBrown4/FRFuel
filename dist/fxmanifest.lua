0227f990aa245faa4f1b2d106191b8ee611a4817
Brandon Brown <brandon.brown39@gmail.com>
2024-10-09 10:48:33 -0400

    update to fxmanifest

--- dist/fxmanifest.lua
+++ dist/fxmanifest.lua
@@ -1,7 +1,11 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+
+game { 'gta5' }
+
 files { 
     "config.ini",
     "Newtonsoft.Json.dll",
     "GasStations.json"
 }
+
 client_script "frfuel.net.dll"
